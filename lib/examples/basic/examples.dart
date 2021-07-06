dde1f0f4dc30309cf52a85e17e969184201972ab
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-07-06 00:41:15 +0000

    Add null_aware_operator.dart

--- lib/examples/basic/examples.dart
+++ lib/examples/basic/examples.dart
@@ -1,7 +1,9 @@
 import 'package:try_dart/types/mapping/mapping.dart';
 
 import 'helloworld/examples.dart' as helloworld;
+import 'null_safety/examples.dart' as nullsafety;
 
 void build(ExampleMapping map) {
   helloworld.build(map);
+  nullsafety.build(map);
 }
