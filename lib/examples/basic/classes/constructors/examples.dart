846193f881b345b2fe475068d2250d85281d84f6
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-08-24 16:47:44 +0900

    Add Constructor optional parameter example

--- lib/examples/basic/classes/constructors/examples.dart
+++ lib/examples/basic/classes/constructors/examples.dart
@@ -1,6 +1,8 @@
+import 'package:try_dart/examples/basic/classes/constructors/optional.dart';
 import 'package:try_dart/examples/basic/classes/constructors/positional.dart';
 import 'package:try_dart/types/mapping/mapping.dart';
 
 void build(ExampleMapping map) {
   map.add('basic_classes_constructors_positional', positional);
+  map.add('basic_classes_constructors_optional', optional);
 }
