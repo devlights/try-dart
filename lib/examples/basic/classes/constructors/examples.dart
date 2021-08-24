f5da5fd53fab9ebdd646bd728a2c9a8c3dd3fb5e
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-08-24 16:44:14 +0900

    Add Constructor example

---
+++ lib/examples/basic/classes/constructors/examples.dart
@@ -0,0 +1,6 @@
+import 'package:try_dart/examples/basic/classes/constructors/positional.dart';
+import 'package:try_dart/types/mapping/mapping.dart';
+
+void build(ExampleMapping map) {
+  map.add('basic_classes_constructors_positional', positional);
+}
--- lib/examples/basic/examples.dart
+++ lib/examples/basic/examples.dart
@@ -1,11 +1,13 @@
 import 'package:try_dart/types/mapping/mapping.dart';
 
+import 'classes/constructors/examples.dart' as classes;
 import 'helloworld/examples.dart' as helloworld;
 import 'null_safety/examples.dart' as nullsafety;
 import 'stacktrace/examples.dart' as stacktrace;
 import 'strings/examples.dart' as strings;
 
 void build(ExampleMapping map) {
+  classes.build(map);
   helloworld.build(map);
   nullsafety.build(map);
   stacktrace.build(map);
