c599247b3ddf5e71deb4fbfb185317b4190fa40e
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-08-24 16:52:28 +0900

    Add Constructor named parameter example

--- lib/examples/basic/classes/constructors/examples.dart
+++ lib/examples/basic/classes/constructors/examples.dart
@@ -1,3 +1,4 @@
+import 'package:try_dart/examples/basic/classes/constructors/named.dart';
 import 'package:try_dart/examples/basic/classes/constructors/optional.dart';
 import 'package:try_dart/examples/basic/classes/constructors/positional.dart';
 import 'package:try_dart/types/mapping/mapping.dart';
@@ -5,4 +6,5 @@ import 'package:try_dart/types/mapping/mapping.dart';
 void build(ExampleMapping map) {
   map.add('basic_classes_constructors_positional', positional);
   map.add('basic_classes_constructors_optional', optional);
+  map.add('basic_classes_constructors_named', named);
 }
